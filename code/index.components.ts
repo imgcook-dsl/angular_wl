0539985f3f7a307c0ac091f20a9995e89e23fec6
wanglei WX5328151 <wanglei484@huawei.com>
2021-02-02 10:17:42 +0800

    angular生成模板提交

--- code/index.components.ts
+++ code/index.components.ts
@@ -1,7 +1,47 @@
-import {Component, OnInit, OnDestroy} from '@angular/core'; @Component({ selector: 'app-index', templateUrl:
-'./index.component.html', styleUrls: ['./index.component.less'] }) export class IndexComponent implements OnInit,
-OnDestroy {
-"data":[{"title":"小户型卫浴怎样才能装得高大上？","coverImage":"https://img.alicdn.com/tfs/TB1Txq6o7T2gK0jSZFkXXcIQFXa-684-684.png","readCount":200,"user":{"userImage":"https://img.alicdn.com/tfs/TB1DWe6oYj1gK0jSZFOXXc7GpXa-60-60.png","userName":"时尚家居"},"url":"https://www.imgcook.com"},{"title":"拥有超多功能的40平米简约小公寓了解一下","coverImage":"https://img.alicdn.com/tfs/TB1XRQTo7P2gK0jSZPxXXacQpXa-684-648.png","readCount":500,"user":{"userImage":"https://img.alicdn.com/tfs/TB1DWe6oYj1gK0jSZFOXXc7GpXa-60-60.png","userName":"花花设计工作"},"url":"https://www.imgcook.com/docs"}]
-constructor() { console.log('super props'); } ngOnInit() { console.log('ngOnInit props'); } ngOnDestroy() {
-console.log('ngOnDestroy props'); } isReadCountShow(readCount) { return readCount > 300; } onClick_1(e) {
-window.open(e.url, '_blank') } }
+import { Component, OnInit, OnDestroy } from "@angular/core";
+@Component({
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.less"]
+})
+export class IndexComponent implements OnInit, OnDestroy {
+  data: [
+    {
+      title: "小户型卫浴怎样才能装得高大上？",
+      coverImage: "https://img.alicdn.com/tfs/TB1Txq6o7T2gK0jSZFkXXcIQFXa-684-684.png",
+      readCount: 200,
+      user: {
+        userImage: "https://img.alicdn.com/tfs/TB1DWe6oYj1gK0jSZFOXXc7GpXa-60-60.png",
+        userName: "时尚家居"
+      },
+      url: "https://www.imgcook.com"
+    },
+    {
+      title: "拥有超多功能的40平米简约小公寓了解一下",
+      coverImage: "https://img.alicdn.com/tfs/TB1XRQTo7P2gK0jSZPxXXacQpXa-684-648.png",
+      readCount: 500,
+      user: {
+        userImage: "https://img.alicdn.com/tfs/TB1DWe6oYj1gK0jSZFOXXc7GpXa-60-60.png",
+        userName: "花花设计工作"
+      },
+      url: "https://www.imgcook.com/docs"
+    }
+  ];
+
+  constructor() {
+    console.log("super props");
+  }
+  ngOnInit() {
+    console.log("ngOnInit props");
+  }
+  ngOnDestroy() {
+    console.log("ngOnDestroy props");
+  }
+
+  isReadCountShow(readCount) {
+    return readCount > 300;
+  }
+  onClick_1(e) {
+    window.open(e.url, "_blank");
+  }
+}
--- src/index.js
+++ src/index.js
@@ -453,36 +453,6 @@ module.exports = function(schema, option) {
         panelValue: prettier.format(`${styles.join('\n')}`, { parser: 'less' }),
         panelType: 'less'
       },
-      // {
-      //   panelName: 'index.components.spec.ts',
-      //   panelValue: prettier.format(
-      //     `
-      //     import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-      //     import { IndexComponent } from './index.component';
-
-      //     describe('IndexComponent', () => {
-      //       let component: IndexComponent;
-      //       let fixture: ComponentFixture<IndexComponent>;
-
-      //       beforeEach(async(() => {
-      //         TestBed.configureTestingModule({
-      //           declarations: [ IndexComponent ]
-      //         }).compileComponents();
-      //       }));
-
-      //       beforeEach(() => {
-      //         fixture = TestBed.createComponent(IndexComponent);
-      //         component = fixture.componentInstance;
-      //         fixture.detectChanges();
-      //       });
-
-      //       it('should create', () => {
-      //         expect(component).toBeTruthy();
-      //       });
-      //     });
-      //   `, prettierOpt),
-      //   panelType: 'ts'
-      // },
       {
         panelName: 'index.components.ts',
         panelValue: prettier.format(`
@@ -502,7 +472,9 @@ module.exports = function(schema, option) {
             
             }
           `,
-          prettierOpt),
+          {
+            parser: 'babel'
+          }),
           panelType: 'js',
       }
     ],
